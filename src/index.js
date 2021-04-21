818ddd7808b2d458d04fa70f8c573bb8b03bdcfe
Anthony <anthonykmbanks@gmail.com>
2021-04-21 08:35:05 -0400

    Completd Assignment

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ClickityClick from './components/ClickityClick'
+import ButtonCounter from './components/ButtonCounter'
+import LightSwitch from './components/LightSwitch'
 
 ReactDOM.render(
-  <div>
-    Mount Components Here
+  
+  <div>Mount Components Here
+  <ClickityClick />, document.getElementById('root')
+  <LightSwitch />
+  <ButtonCounter />
+
   </div>,
   document.getElementById('root')
 );
